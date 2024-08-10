13b3a61f95bc7f7502646c9637a552fca895fdc7
Arpitpandey992 <arpitpandey992@gmail.com>
2024-08-10 21:30:13 +0530

    refactor: move tests inside unigen

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="unigen",
-    version="1.4.2",
+    version="1.4.2.rev1",
     packages=find_packages(),
     install_requires=[
         "mutagen",
