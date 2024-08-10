0c6a7809b96c364d544bba00cc3f582f622fb14d
Arpitpandey992 <arpitpandey992@gmail.com>
2024-08-10 21:17:19 +0530

    release: bump up version to 1.4.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="unigen",
-    version="1.4.1.dev1",
+    version="1.4.2",
     packages=find_packages(),
     install_requires=[
         "mutagen",
