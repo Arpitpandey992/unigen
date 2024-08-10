eb3a54a8202d6708450d3a822540d39d630f3405
Arpitpandey992 <arpitpandey992@gmail.com>
2024-08-10 21:46:57 +0530

    fix: add __init__.py to tests but remove the package from packages in setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,13 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as f:
     description = f.read()
 
+packages = find_packages()
+packages.remove("unigen.tests")  # remove tests to avoid bloating the package
+
 setup(
     name="unigen",
     version="1.4.2.post2",
-    packages=find_packages(),
+    packages=packages,
     install_requires=[
         "mutagen",
     ],
