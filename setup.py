7d40df0e8e1fa9adc3b472d428e2e5bcab7c8043
Arpitpandey992 <arpitpandey992@gmail.com>
2024-08-10 21:01:10 +0530

    chore: change version number to conform to PEP 440

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="unigen",
-    version="1.4.1.test-with-passing-tests",
+    version="1.4.1.dev1",
     packages=find_packages(),
     install_requires=[
         "mutagen",
