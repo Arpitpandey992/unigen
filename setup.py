a7742e90bb375bd7fd93eed5068a078f6b5281cc
Arpitpandey992 <arpitpandey992@gmail.com>
2024-08-10 20:51:24 +0530

    feat: ensure pytest is passing before publishing to pypi, deliberately fail one test to ensure it is working

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="unigen",
-    version="1.4.1",
+    version="1.4.1.test",
     packages=find_packages(),
     install_requires=[
         "mutagen",
