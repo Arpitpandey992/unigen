268f974bcaa1dca8efe4ef5d0d715cda3cdeb304
Arpitpandey992 <arpitpandey992@gmail.com>
2024-08-10 22:38:40 +0530

    feat: export the metadata objects

--- unigen/__init__.py
+++ unigen/__init__.py
@@ -1,4 +1,5 @@
 from .types.picture import PICTURE_NAME_TO_NUMBER, PICTURE_TYPE
+from .types.audio_metadata import AudioFileMetadata, MediaInfo, Tags
 from .wrapper.audio_factory import (
     AudioFactory,
     UnsupportedFileFormatError,
